3b9e963ebfd51bd8a27081022d62ffd879e2db59
Alex <secret.teamplayer@gmail.com>
2026-02-03 19:24:04 +0200

    Fixed logic for SuperSortPropsType. pureChange correct sort.

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -1,25 +1,36 @@
 import React from 'react'
+import downArrow  from './icons/si--expand-more-duotone.svg'
+import upArrow  from './icons/si--expand-less-duotone.svg'
+import doubleArrow  from './icons/si--up-down-duotone.svg'
 
 // добавить в проект иконки и импортировать
-const downIcon = '[\\/]'
-const upIcon = '[/\\]'
-const noneIcon = '[--]'
+// const DownIcon = FaSortDown as React.ElementType
+// const UpIcon = FaSortUp as React.ElementType
+// const noneIcon = FaSort as React.ElementType
+
+const downIcon = downArrow
+const upIcon = upArrow
+const noneIcon = doubleArrow
 
 export type SuperSortPropsType = {
     id?: string
     sort: string
     value: string
     onChange: (newSort: string) => void
+    className?: string
 }
 
 export const pureChange = (sort: string, down: string, up: string) => {
+    if(sort === '') return down
+    if(sort === down) return up
+    if(sort === up) return ''
     // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
+    return down
 }
 
 const SuperSort: React.FC<SuperSortPropsType> = (
     {
-        sort, value, onChange, id = 'hw15',
+        sort, value, onChange, className, id = 'hw15',
     }
 ) => {
     const up = '0' + value
@@ -35,18 +46,24 @@ const SuperSort: React.FC<SuperSortPropsType> = (
             ? upIcon
             : noneIcon
 
+    const styles = className ? className : ''
+
+
     return (
         <span
             id={id + '-sort-' + value}
             onClick={onChangeCallback}
+            // className={styles}
+            style={{display: "flex"}}
         >
+            <img id={id + '-icon-' + sort} src={icon} alt={'some sort'} style={{alignItems: 'center'}}/>
             {/*сделать иконку*/}
             {/*<img*/}
             {/*    id={id + '-icon-' + sort}*/}
             {/*    src={icon}*/}
             {/*/>*/}
 
-            {icon} {/*а это убрать*/}
+            {/*{icon} /!*а это убрать*!/*/}
         </span>
     )
 }
